06d59981a70a3074f0d12b52dd7242fa47c8785f
kleefi <decharvi.charles@gmail.com>
2021-11-08 14:48:09 +0700

    myrep

--- index.js
+++ index.js
@@ -4,83 +4,23 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const newspapers = [
+const artikels = [
     {
-        name: 'cityam',
-        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
-        base: ''
-    },
-    {
-        name: 'thetimes',
-        address: 'https://www.thetimes.co.uk/environment/climate-change',
-        base: ''
-    },
-    {
-        name: 'guardian',
-        address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: '',
-    },
-    {
-        name: 'telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk',
-    },
-    {
-        name: 'nyt',
-        address: 'https://www.nytimes.com/international/section/climate',
-        base: '',
-    },
-    {
-        name: 'latimes',
-        address: 'https://www.latimes.com/environment',
-        base: '',
-    },
-    {
-        name: 'smh',
-        address: 'https://www.smh.com.au/environment/climate-change',
-        base: 'https://www.smh.com.au',
-    },
-    {
-        name: 'un',
-        address: 'https://www.un.org/climatechange',
-        base: '',
-    },
-    {
-        name: 'bbc',
-        address: 'https://www.bbc.co.uk/news/science_and_environment',
-        base: 'https://www.bbc.co.uk',
-    },
-    {
-        name: 'es',
-        address: 'https://www.standard.co.uk/topic/climate-change',
-        base: 'https://www.standard.co.uk'
-    },
-    {
-        name: 'sun',
-        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
-        base: ''
-    },
-    {
-        name: 'dm',
-        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
-        base: ''
-    },
-    {
-        name: 'nyp',
-        address: 'https://nypost.com/tag/climate-change/',
+        name: 'media',
+        address: 'https://mediakonsumen.com/?s=myrepublic#gsc.tab=0',
         base: ''
     }
 ]
 
 const articles = []
 
-newspapers.forEach(newspaper => {
+artikels.forEach(newspaper => {
     axios.get(newspaper.address)
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {
+            $('a:contains("MyRepublic")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
@@ -95,33 +35,29 @@ newspapers.forEach(newspaper => {
 })
 
 app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change News API')
-})
-
-app.get('/news', (req, res) => {
     res.json(articles)
 })
 
-app.get('/news/:newspaperId', (req, res) => {
-    const newspaperId = req.params.newspaperId
+app.get('/artikel/:artikelsId', (req, res) => {
+    const artikelsId = req.params.artikelsId
 
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
-    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+    const artikelAddress = artikels.filter(newspaper => newspaper.name == artikelsId)[0].address
+    const artikelBase = artikels.filter(newspaper => newspaper.name == artikelsId)[0].base
 
 
-    axios.get(newspaperAddress)
+    axios.get(artikelAddress)
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
             const specificArticles = []
 
-            $('a:contains("climate")', html).each(function () {
+            $('a:contains("MyRepublic")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
                 specificArticles.push({
                     title,
-                    url: newspaperBase + url,
-                    source: newspaperId
+                    url: artikelBase + url,
+                    source: artikelsId
                 })
             })
             res.json(specificArticles)
